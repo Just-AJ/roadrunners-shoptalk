efc4045612693f7860550d1832d2d473e4d84a4e
DJMIKE20 <m.zimm20@gmail.com>
2022-10-18 20:44:12 -0400

    updated server.js with ports, helpers, sessions, and handlebars

---
+++ server.js
@@ -0,0 +1,48 @@
+// imports 
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+// accessing PORT and express 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+// setup express session and connects session to database 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+app.use(session(sess));
+
+// import helper functions 
+const helpers = require('./utils/helpers');
+
+// handlebars engine and setup
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+// adding middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(require('./controllers/'));
+
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening to Around the World!'));
+});
