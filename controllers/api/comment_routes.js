6a9de64936bc28810c95d2cdb013e1123be7c948
Brayden <banjosquash@gmail.com>
2022-10-18 20:21:54 -0400

    comment model finished

---
+++ controllers/api/comment_routes.js
@@ -0,0 +1,6 @@
+// import all models
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Comment = require('../../models/Comment');
+
+Comment
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS around _the_world_db;
+
+CREATE DATABASE around _the_world_db;
