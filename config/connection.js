61513d84c837d759a752575217826c520d9bfd53
DJMIKE20 <m.zimm20@gmail.com>
2022-10-18 20:05:05 -0400

    added connection.js for seqeulize

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,22 @@
+// import the sequelize constructor 
+const Sequelize = require('sequelize');
 
+// import env
+require('dotenv').config();
+
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // create connection to our database 
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+
+
+module.exports = sequelize;
