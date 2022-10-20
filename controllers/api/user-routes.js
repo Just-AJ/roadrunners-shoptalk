383a2d65b8c99c5caf33cbf1ec41efc6ae2a6212
DJMIKE20 <m.zimm20@gmail.com>
2022-10-19 20:00:30 -0400

    refactor user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -30,21 +30,7 @@ router.get('/:id', (req, res) => {
             exclude: ['password']
         },
         // get the id parameter
-        where: { id: req.params.id },
-        include: [
-            {
-                model: Post,
-                attributes: ['id', 'title', 'copy', 'created_at']
-            },
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'created_at'],
-                include: {
-                    model: Post,
-                    attributes: ['title']
-                }
-            }
-        ]
+        where: { id: req.params.id }
     })
 
         .then(singleUserData => {
