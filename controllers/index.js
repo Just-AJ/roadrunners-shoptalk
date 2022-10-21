c774a7bfdae84397c76960aaba1b26a3496096b2
DJMIKE20 <m.zimm20@gmail.com>
2022-10-21 13:46:02 -0400

    refactor main and homepage handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,11 @@ const router = require('express').Router();
 // import routes
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js')
+// const dashboardRoutes = require('./dashboard-routes.js')
 
 // use routes 
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+// router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req,res) => {
