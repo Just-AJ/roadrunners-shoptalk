c62adca20070ded3c983ad2014b878ca83dadd58
DJMIKE20 <m.zimm20@gmail.com>
2022-10-27 13:43:28 -0400

    fixed comment page, modal , and refactor forum

---
+++ public/javascript/comment.js
@@ -0,0 +1,49 @@
+
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    console.log("new comment", comment_text);
+    console.log("postID", post_id);
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({
+            post_id,
+            comment_text
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      
+        if (response.ok) {
+          document.location.reload();
+        } else {
+          alert(response.statusText);
+        }
+     } 
+    //  else if (comment_text) {
+    //     const res = await fetch ('/api/comments', {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //           }
+    //     });
+    //     if (res.ok) {
+    //         document.location.reload();
+    //       } else {
+    //         alert(res.statusText);
+    //       }
+
+    // }
+
+}
+
+
+document.querySelector('.post-comment').addEventListener('submit', commentFormHandler);
